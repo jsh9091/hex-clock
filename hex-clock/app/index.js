91b978f8ce7da22e206fbc7baba8cc6927d16532
Joshua Horvath <jsh_197@hotmail.com>
2025-04-06 16:06:39 -0400

    Basic clock display

--- hex-clock/app/index.js
+++ hex-clock/app/index.js
@@ -1,1 +1,55 @@
-console.log('Hello world!');
+import clock from "clock";
+import * as document from "document";
+import { me as appbit } from "appbit";
+import { preferences } from "user-settings";
+
+// Update the clock every second
+clock.granularity = "minutes";
+
+// Get a handle on the <text> elements
+const clockLabel = document.getElementById("clockLabel");
+
+
+/**
+ * Update the display of clock values.
+ * @param {*} evt 
+ */
+clock.ontick = (evt) => {
+  
+    // get time information from API
+    let todayDate = evt.date;
+    let rawHours = todayDate.getHours();
+  
+    let hours;
+    if (preferences.clockDisplay === "12h") {
+      // 12 hour format
+      hours = rawHours % 12 || 12;
+    } else {
+      // 24 hour format
+      if (rawHours > 9) {
+        hours = zeroPad(rawHours);
+      } else {
+        hours = rawHours;
+      }
+    }
+  
+    let mins = todayDate.getMinutes();
+    let displayMins = zeroPad(mins);
+  
+    // display time on main clock
+    clockLabel.text = `${hours}:${displayMins}`;
+
+  };
+
+  
+/**
+ * Front appends a zero to an integer if less than ten.
+ * @param {*} i 
+ * @returns 
+ */
+function zeroPad(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
