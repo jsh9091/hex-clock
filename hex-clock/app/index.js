3645a2defe36e025abd325fdc360fde5ccc667a6
Joshua Horvath <jsh_197@hotmail.com>
2025-04-10 10:41:18 -0400

    Minor code cleanup

--- hex-clock/app/index.js
+++ hex-clock/app/index.js
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Joshua Horvath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import clock from "clock";
 import * as document from "document";
 import { me as appbit } from "appbit";
@@ -7,10 +31,9 @@ import { preferences } from "user-settings";
 clock.granularity = "minutes";
 
 // Get a handle on the <text> elements
-const clockLabel = document.getElementById("clockLabel");
+const digitalClockLabel = document.getElementById("digitalClockLabel");
 let hourHand = document.getElementById("hourHand");
 let minuteHand = document.getElementById("minuteHand");
-
 
 /**
  * Update the display of clock values.
@@ -27,15 +50,10 @@ clock.ontick = (evt) => {
 
     let mins = todayDate.getMinutes();
 
-    // convert to hex
-    let dhours = hours.toString(16);
-    let dmins = mins.toString(16);
+    // display time on main digital clock
+    digitalClockLabel.text = `${decimalToHexString(hours)}:${decimalToHexString(mins)}`;
 
-
-    // display time on main clock
-    clockLabel.text = `${decimalToHexString(hours)}:${decimalToHexString(mins)}`;
-
-    updateClock();
+    updateAnalogClock();
 };
 
 /**
@@ -47,33 +65,18 @@ function decimalToHexString(number) {
   return number.toString(16).toUpperCase();
 }
 
-  
 /**
- * Front appends a zero to an integer if less than ten.
- * @param {*} i 
- * @returns 
- */
-function zeroPad(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-
-  /**
  * Rotates the clock hands to show the curent time.
  */
-function updateClock() {
+function updateAnalogClock() {
     let today = new Date();
     let hours = today.getHours() % 12;
     let mins = today.getMinutes();
-    let secs = today.getSeconds();
   
     hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
     minuteHand.groupTransform.rotate.angle = minutesToAngle(mins);
   }
 
-  
 /**
  * Returns an angle (0-360) for the current hour in the day.
  * Also adjust the hour hand for minutes past the hour.
